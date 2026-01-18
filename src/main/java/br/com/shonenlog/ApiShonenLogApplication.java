a8ff472e66d3448692099b84f5e434e37dd2bfd7
Eduardo Caetano <eduardocaetano110@gmail.com>
2026-01-18 13:26:04 -0300

    good first commit

---
+++ src/main/java/br/com/shonenlog/ApiShonenLogApplication.java
@@ -0,0 +1,13 @@
+package br.com.shonenlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiShonenLogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiShonenLogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/shonenlog/ApiShonenLogApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.shonenlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiShonenLogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
