2fc0fe9b1d0e398b450c1a30a458f4e74b0e1a31
Eduardo Caetano <eduardocaetano110@gmail.com>
2026-01-18 17:03:31 -0300

    feat: create entity Category

---
+++ src/main/java/br/com/shonenlog/entity/Category.java
@@ -0,0 +1,22 @@
+package br.com.shonenlog.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "category")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 100, nullable = false)
+    private String name;
+}
