c6ba8bbc13dad34541d2a7561b8913c3216a2db5
Eduardo Caetano <eduardocaetano110@gmail.com>
2026-01-20 20:27:19 -0300

    refactor(entity): add @Builder to Category

--- src/main/java/br/com/shonenlog/entity/Category.java
+++ src/main/java/br/com/shonenlog/entity/Category.java
@@ -1,10 +1,7 @@
 package br.com.shonenlog.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Entity
 @Table(name = "category")
@@ -12,6 +9,7 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
